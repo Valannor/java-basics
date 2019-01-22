a21d3a8b2ddf3e0c96f402e2a2b10123cb932fdb
Rustam Gurd <rustamkurdov@mail.ru>
2019-01-22 16:13:48 +0300

    ArrayList implementation: add, ensureCapacity

--- src/main/java/com/practice/TestApp.java
+++ src/main/java/com/practice/TestApp.java
@@ -1,4 +1,29 @@
 package com.practice;
 
+import com.practice.collection.MyArrayList;
+
+import java.util.ArrayList;
+
 public class TestApp {
+
+    public static void main(String[] args) {
+        String[] strings = {"a", "b", "c", "d", "e"};
+
+        MyArrayList<String> list = new MyArrayList<>();
+        list.add("New 1");
+        list.add("New 2");
+        list.add("New 3");
+        list.add("New 4");
+        list.add("New 5");
+        list.add("New 6");
+        list.add("New 7");
+        list.add("New 8");
+        list.add("New 9");
+        list.add("New 10");
+        list.add("New 11");
+        list.add("New 12");
+
+        System.out.println(list);
+        System.out.println(list.size());
+    }
 }
--- src/main/java/com/practice/collection/MyArrayList.java
+++ src/main/java/com/practice/collection/MyArrayList.java
@@ -1,13 +1,20 @@
 package com.practice.collection;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
-public class MyArrayList<E> implements Iterable{
+public class MyArrayList<E> {
+
+    private static final int DEFAULT_CAPACITY = 10;
 
     private E[] elements;
 
     private int capacity;
     private int size;
+
+    public MyArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
 
     public MyArrayList(int capacity) {
         this.capacity = capacity;
@@ -22,13 +29,46 @@ public class MyArrayList<E> implements Iterable{
         System.arraycopy(source, 0, this.elements, 0, source.length);
     }
 
-
-    // TODO: 22.01.2019
-    @Override
-    public Iterator iterator() {
-
-        return null;
+    public void add(E element) {
+        ensureCapacity();
+        elements[size] = element;
+        size++;
     }
 
+    private void ensureCapacity() {
+        if (size >= capacity) {
+            capacity = (capacity * 3) / 2 + 1;
 
+            E[] temp = (E[]) new Object[capacity];
+            addAll(elements, temp);
+            elements = temp;
+        }
+    }
+
+    private void addAll(E[] src, E[] dest) {
+        System.arraycopy(src, 0, dest, 0, src.length);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("[");
+        for (int i = 0; i < size; i++) {
+
+            builder.append(elements[i]);
+
+            if (i < size - 1) {
+                builder.append(", ");
+            } else
+                builder.append("]");
+        }
+
+        return builder.toString();
+    }
+
+    public int size() {
+        return size;
+    }
 }
